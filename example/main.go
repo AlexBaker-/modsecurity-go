5d32649ea2b2f3d0046a5a6c05b1840df200eb04
Sandor Szücs <sandor.szuecs@zalando.de>
2019-06-01 19:26:05 +0200

    add an example and import files you need in order to build and run it
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

---
+++ example/main.go
@@ -0,0 +1,33 @@
+package main
+
+// #cgo CFLAGS: -I/server/local/include
+// #cgo LDFLAGS: -L/server/local/lib -lmodsecurity
+
+import (
+	"github.com/sirupsen/logrus"
+
+	"github.com/senghoo/modsecurity-go/libmodsecurity"
+)
+
+const (
+	rulesFile = "basic_rules.conf"
+)
+
+func main() {
+	lms := libmodsecurity.NewLibModSecurity()
+	if err := lms.AddRuleFromFile(rulesFile); err != nil {
+		logrus.Errorf("Failed to load %s: %v", rulesFile, err)
+	}
+	tr := lms.NewTransaction()
+	tr.ProcessConnection("127.0.0.1", "127.0.0.1", 12345, 80)
+	tr.ProcessURL(
+		"http://www.modsecurity.org/test?key1=value1&key2=value2&key3=value3",
+		"GET",
+		1,
+		1)
+	tr.ProcessRequestHeader()
+	tr.ProcessRequestBody()
+	tr.ProcessResponseHeader()
+	tr.ProcessResponseBody()
+
+}
